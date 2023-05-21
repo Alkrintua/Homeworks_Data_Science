28e28e01cb4c12ca7a1f1d6e4ce2bc5c7fd85154
Alkrint <alkrintua@gmail.com>
2023-05-21 17:23:19 +0300

    002 adding training code with class car

---
+++ lesson_09/trn_09.py
@@ -0,0 +1,16 @@
+# create a car class
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+    def __str__(self):
+        return f"The {self.color} car has {self.mileage} miles."
+
+
+blue_car = Car("blue", 20000)
+red_car = Car("red", 30000)
+print(blue_car)
+print(red_car)
+
+
